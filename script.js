0b8778bbbe2e8953aac35a5390a879b324c92010
bruno gomes <icestonebruno@gmail.com>
2021-02-13 12:16:06 -0300

    copy function

--- script.js
+++ script.js
@@ -52,3 +52,11 @@ function generate(){
 function revert(s){
     return s.split('').reverse('').join('');
 }
+
+function myFunction() {
+  /* Get the text field */
+  var copyText = document.getElementById("copy");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999); /* For mobile devices */
+  document.execCommand("copy");
+}
