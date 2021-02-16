7439c140c5a7158fac7331fc04e4420e97ddf8db
Bruno Gomes <icestonebruno@gmail.com>
2021-02-16 12:52:47 -0300

    correção copy

--- script.js
+++ script.js
@@ -108,9 +108,9 @@ function generate(){
 }
 function myFunction() {
   /* Get the text field */
-  var copyText = document.getElementById("copy");
+  var copyText = document.getElementById("resultado");
   copyText.select();
   copyText.setSelectionRange(0, 99999); /* For mobile devices */
-  document.execCommand("copy");
+  document.execCommand("resultado");
 }
 
